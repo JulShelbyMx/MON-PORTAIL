4a9fef36fdbb0afdb38d3210d7013509ea638880
JulShelbyMx <110037768+JulShelbyMx@users.noreply.github.com>
2025-04-14 01:58:46 +0200

    op maj test 2

--- Streaming/streaming.js
+++ Streaming/streaming.js
@@ -1260,7 +1260,7 @@ function generateStreamingLinks() {
         episodes: [
         { episode: 1123, videoUrl: 'https://video.sibnet.ru/shell.php?videoid=5869127'},
         { episode: 1124, videoUrl: 'https://video.sibnet.ru/shell.php?videoid=5870323'},
-        { episode: 1125, videoUrl: 'http://video.sibnet.ru/shell.php?videoid=5878182'},
+        { episode: 1125, videoUrl: 'https://video.sibnet.ru/shell.php?videoid=5878182'},
         { episode: 1126, videoUrl: ''},
 
     ]
