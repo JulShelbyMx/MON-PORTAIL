97f7bc0f14571eeb17dc13852a81513d2a47cc21
JulShelbyMx <thomasjuleseliot@gmail.com>
2025-04-28 20:18:55 +0200

    popq

--- manga/mangalinks.py
+++ manga/mangalinks.py
@@ -191,8 +191,3 @@ for chapter in all_chapters:
 js_code += "        ]\n"
 js_code += "    }\n"
 js_code += "];"
-
-# Sauvegarder le code JavaScript dans un fichier
-with open(r"C:\Users\Eleve\OneDrive - LYCEE FREDERIC OZANAM\Documents\MON PORTAIL\Streaming_onepiece\streaming.js", "w") as f:
-    f.write(js_code)
-print(f"Code JavaScript généré et sauvegardé dans D:\manga\streaming_code.js")
